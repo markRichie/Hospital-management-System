c1a2c255f55835131fbb5fef660613857bdc7916
unknown <markrichie96@gmail.com>
2019-12-27 23:52:13 +0530

    added updated db

---
+++ admissions.php
@@ -0,0 +1,169 @@
+<?php 
+session_start();
+//echo $_SESSION['color']; 
+$rle = $_SESSION["role"];
+?> 
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+
+    <title>Hello, world!</title>
+    <style>
+      .headg{
+        margin-left: 30px;
+        margin-right: 30px;
+      }
+      h1{
+        margin-top: 20px;
+        margin-bottom: 20px;
+      }
+      #ph{
+        margin-top: 5px;
+      }
+      #nw{
+        margin-bottom: 20px;
+      }
+    </style>
+    <script>
+      function al(){
+        modal.style.display = "block";
+      }
+      function tr(uid){
+        //alert("voila")
+        window.location.href="http://localhost/MSS/reports.php?id="+uid;
+      }
+      function insertParam(key, value)
+      {
+        key = encodeURI(key); value = encodeURI(value);
+        var kvp = document.location.search.substr(1).split('&');
+
+        var i=kvp.length; var x; while(i--) 
+        {
+            x = kvp[i].split('=');
+
+            if (x[0]==key)
+            {
+                x[1] = value;
+                kvp[i] = x.join('=');
+                break;
+            }
+        }
+
+        if(i<0) {kvp[kvp.length] = [key,value].join('=');}
+
+        //this will reload the page, it's likely better to store this until finished
+        document.location.search = kvp.join('&'); 
+      }
+    </script>
+  </head>
+  <body>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <h5 style="color: #808281;">Asceso Hospital</h5>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor03" aria-controls="navbarColor03" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarColor03">
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item active">
+          <a class="nav-link" href="#"><span class="sr-only">(current)</span></a>
+        </li>
+      </ul>
+      <form class="form-inline my-2 my-lg-0">
+        <button class="btn btn-info my-2 my-sm-0" type="submit">Sign out</button>
+      </form>
+    </div>
+  </nav>
+  <div class="headg" >
+    <h1>Admissions</h1>
+    <div class="alert alert-dismissible alert-info">
+    <strong>front office clerk : </strong>
+    </div>
+    
+    <button type="button" id = "nw" class="btn btn-info" data-toggle="modal" data-target="#new_admsn">New</button>
+
+    <table class="table table-hover">
+      <thead>
+        <tr>
+          <th scope="col">Admission no</th>
+          <th scope="col">Patient Name</th>
+          <th scope="col">Ward no</th>
+          <th scope="col">Bed no</th>
+          <th scope="col">actions</th>
+        </tr>
+      </thead>
+      <!-- replace php code with config when common database-->
+      <tbody>
+        <?php
+
+          $conn = mysqli_connect("localhost", "root", "","hospital_db");
+          $query = mysqli_query($conn,"select * from admission where status = 'admitted'");
+          //echo $query;
+          
+
+          //$num = 1;
+          while ($row = mysqli_fetch_array($query)) {
+            echo"<tr>";
+              //echo"<td>".$num."</td>";
+              //$nme = $row['NIC'];
+              //$query1 = mysqli_query($conn,"select * from patient where NIC = '$nme'");
+              //$row1 = mysqli_fetch_array($query1);
+              echo"<td>".$row['ad_no']."</td>";
+              echo"<td>".$row['NIC']."</td>";
+              echo"<td>".$row['ward_no']."</td>";
+              echo"<td>".$row['bed_no']."</td>";
+              echo"<td><button type='button' class='btn btn-info'>Discharge</button></td>";
+            echo"</tr>";
+          }
+        ?>
+      </tbody>
+    </table> 
+    
+
+    <form class="form-group" action="functions.php"  method="POST">
+        <div class="modal" id="new_admsn">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title">New Admission</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                
+                            <h6>Symtoms</h6>
+                            <textarea class="form-control" name="sytm" id="exampleTextarea" rows="3"></textarea>
+                            <h6>Diagnosis</h6>
+                            <textarea class="form-control" name="dia" id="exampleTextarea" rows="3"></textarea>
+                            <h6>Change details</h6>
+                            <textarea class="form-control" name="c_d" id="exampleTextarea" rows="3"></textarea>
+                            <h6>Remarks</h6>
+                            <textarea class="form-control" name="rmks" id="exampleTextarea" rows="3"></textarea>
+                            
+                            
+            </div>  
+            <div class="modal-footer">
+                <button type="submit" class="btn btn-primary" name="add_history">Save changes</button>
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+            </div>
+            </div>
+        </div>
+        </div>
+    </form>
+  
+
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+  </body>
+</html>
--- hospital_db.sql
+++ hospital_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Dec 16, 2019 at 03:33 PM
+-- Generation Time: Dec 27, 2019 at 06:19 PM
 -- Server version: 10.4.10-MariaDB
 -- PHP Version: 7.3.12
 
@@ -25,12 +25,36 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `admission`
+--
+
+DROP TABLE IF EXISTS `admission`;
+CREATE TABLE IF NOT EXISTS `admission` (
+  `ad_no` int(11) NOT NULL AUTO_INCREMENT,
+  `NIC` varchar(10) NOT NULL,
+  `ward_no` int(11) NOT NULL,
+  `bed_no` int(11) NOT NULL,
+  `status` text NOT NULL,
+  `d_id` int(11) NOT NULL,
+  PRIMARY KEY (`ad_no`)
+) ENGINE=MyISAM AUTO_INCREMENT=4001 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `admission`
+--
+
+INSERT INTO `admission` (`ad_no`, `NIC`, `ward_no`, `bed_no`, `status`, `d_id`) VALUES
+(4000, '123456789v', 4, 2, 'admitted', 2001);
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `appointment`
 --
 
 DROP TABLE IF EXISTS `appointment`;
 CREATE TABLE IF NOT EXISTS `appointment` (
-  `a_id` int(11) NOT NULL,
+  `a_id` int(11) NOT NULL AUTO_INCREMENT,
   `date` date DEFAULT NULL,
   `time` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
   `d_id` int(11) DEFAULT NULL,
@@ -38,7 +62,15 @@ CREATE TABLE IF NOT EXISTS `appointment` (
   `NIC` varchar(10) DEFAULT NULL,
   PRIMARY KEY (`a_id`),
   KEY `FK` (`d_id`,`c_id`,`NIC`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `appointment`
+--
+
+INSERT INTO `appointment` (`a_id`, `date`, `time`, `d_id`, `c_id`, `NIC`) VALUES
+(1, '2019-12-19', '2019-12-23 05:44:19', 1001, 1, '123456789v'),
+(2, '2019-12-27', '2019-12-23 06:18:26', 1001, 1, '987654321v');
 
 -- --------------------------------------------------------
 
@@ -48,13 +80,19 @@ CREATE TABLE IF NOT EXISTS `appointment` (
 
 DROP TABLE IF EXISTS `doctor`;
 CREATE TABLE IF NOT EXISTS `doctor` (
-  `d_id` int(11) NOT NULL,
+  `d_id` int(11) NOT NULL AUTO_INCREMENT,
   `name` char(50) DEFAULT NULL,
   `fees` double DEFAULT NULL,
-  `username` varchar(20) DEFAULT NULL,
-  `password` varchar(30) DEFAULT NULL,
   PRIMARY KEY (`d_id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=1002 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `doctor`
+--
+
+INSERT INTO `doctor` (`d_id`, `name`, `fees`) VALUES
+(1000, 'sam perera', 1500),
+(1001, 'john fernando', 1200);
 
 -- --------------------------------------------------------
 
@@ -64,12 +102,17 @@ CREATE TABLE IF NOT EXISTS `doctor` (
 
 DROP TABLE IF EXISTS `front office clerk`;
 CREATE TABLE IF NOT EXISTS `front office clerk` (
-  `c_id` int(11) NOT NULL,
+  `c_id` int(11) NOT NULL AUTO_INCREMENT,
   `name` char(50) DEFAULT NULL,
-  `username` varchar(20) DEFAULT NULL,
-  `password` varchar(30) DEFAULT NULL,
   PRIMARY KEY (`c_id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `front office clerk`
+--
+
+INSERT INTO `front office clerk` (`c_id`, `name`) VALUES
+(1, 'sanduni');
 
 -- --------------------------------------------------------
 
@@ -79,12 +122,66 @@ CREATE TABLE IF NOT EXISTS `front office clerk` (
 
 DROP TABLE IF EXISTS `lab_technician`;
 CREATE TABLE IF NOT EXISTS `lab_technician` (
-  `t_id` int(11) NOT NULL,
+  `t_id` int(11) NOT NULL AUTO_INCREMENT,
   `name` char(50) DEFAULT NULL,
-  `username` varchar(20) DEFAULT NULL,
-  `password` varchar(30) DEFAULT NULL,
   PRIMARY KEY (`t_id`)
+) ENGINE=MyISAM AUTO_INCREMENT=2002 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `lab_technician`
+--
+
+INSERT INTO `lab_technician` (`t_id`, `name`) VALUES
+(2000, 'sam wasantha'),
+(2001, 'thilina kumara');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `login`
+--
+
+DROP TABLE IF EXISTS `login`;
+CREATE TABLE IF NOT EXISTS `login` (
+  `password` varchar(30) NOT NULL,
+  `username` varchar(25) NOT NULL,
+  `role` varchar(30) NOT NULL,
+  `id` int(11) NOT NULL,
+  PRIMARY KEY (`username`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `login`
+--
+
+INSERT INTO `login` (`password`, `username`, `role`, `id`) VALUES
+('1234', 'doc1', 'doctor', 1001),
+('1234', 'clerk1', 'front office clerk', 1),
+('1234', 'lab1', 'lab_technician', 2000);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `medicine`
+--
+
+DROP TABLE IF EXISTS `medicine`;
+CREATE TABLE IF NOT EXISTS `medicine` (
+  `m_id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(25) NOT NULL,
+  `qty` int(11) NOT NULL,
+  `price` double NOT NULL,
+  PRIMARY KEY (`m_id`)
+) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `medicine`
+--
+
+INSERT INTO `medicine` (`m_id`, `name`, `qty`, `price`) VALUES
+(1, 'pandol', 45, 10),
+(2, 'piriton', 100, 5),
+(3, 'bandage', 45, 200);
 
 -- --------------------------------------------------------
 
@@ -96,8 +193,18 @@ DROP TABLE IF EXISTS `patient`;
 CREATE TABLE IF NOT EXISTS `patient` (
   `NIC` varchar(10) NOT NULL,
   `name` char(50) DEFAULT NULL,
+  `age` int(11) DEFAULT NULL,
   PRIMARY KEY (`NIC`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `patient`
+--
+
+INSERT INTO `patient` (`NIC`, `name`, `age`) VALUES
+('123456789v', 'supun fernando', 21),
+('987654321v', 'paul george', 16),
+('102345678v', 'donald kumar', 42);
 
 -- --------------------------------------------------------
 
@@ -107,18 +214,29 @@ CREATE TABLE IF NOT EXISTS `patient` (
 
 DROP TABLE IF EXISTS `patient_history`;
 CREATE TABLE IF NOT EXISTS `patient_history` (
-  `his_id` int(11) NOT NULL,
+  `his_id` int(11) NOT NULL AUTO_INCREMENT,
   `NIC` varchar(10) DEFAULT NULL,
   `date_Time` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
   `symtoms` varchar(150) DEFAULT NULL,
   `Diagnosis` char(200) DEFAULT NULL,
   `change details` char(200) DEFAULT NULL,
   `remarks` char(200) DEFAULT NULL,
-  `r_id` int(11) DEFAULT NULL,
   `p_id` int(11) DEFAULT NULL,
+  `r_type` varchar(50) DEFAULT NULL,
   PRIMARY KEY (`his_id`),
-  KEY `FK` (`NIC`,`r_id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+  KEY `FK` (`NIC`)
+) ENGINE=MyISAM AUTO_INCREMENT=24 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `patient_history`
+--
+
+INSERT INTO `patient_history` (`his_id`, `NIC`, `date_Time`, `symtoms`, `Diagnosis`, `change details`, `remarks`, `p_id`, `r_type`) VALUES
+(1, '123456789v', '2019-12-27 07:23:20', 'headache, vomitish, sneezing', 'viral fever', NULL, 'meet after 3 days', NULL, 'scan'),
+(2, '987654321v', '2019-12-27 07:23:11', 'ankle pain, difficult to walk', 'ankle sprain', NULL, 'if pain exceeds 10 days take scan', NULL, 'scan'),
+(3, '777888999v', '2019-12-27 07:22:43', 'fever, dizzyness', 'viral fever', NULL, 'meet me in 3 days if not well', NULL, 'scan'),
+(21, '123456789v', '2019-12-21 17:44:24', 'headache', 'headache', '', '', NULL, 'scan'),
+(23, '222111222v', '2019-12-21 18:12:30', 'fever', 'fever', NULL, NULL, NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -128,7 +246,7 @@ CREATE TABLE IF NOT EXISTS `patient_history` (
 
 DROP TABLE IF EXISTS `report`;
 CREATE TABLE IF NOT EXISTS `report` (
-  `r_id` int(11) NOT NULL,
+  `r_id` int(11) NOT NULL AUTO_INCREMENT,
   `type` char(20) DEFAULT NULL,
   `results` varchar(100) DEFAULT NULL,
   `s_received_date` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
@@ -137,8 +255,32 @@ CREATE TABLE IF NOT EXISTS `report` (
   `fees` double DEFAULT NULL,
   `link_to_softcopy` varchar(200) DEFAULT NULL,
   `t_id` int(11) DEFAULT NULL,
+  `NIC` varchar(10) NOT NULL,
   PRIMARY KEY (`r_id`),
-  KEY `FK` (`t_id`)
+  KEY `FK` (`t_id`),
+  KEY `NIC` (`NIC`)
+) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `report`
+--
+
+INSERT INTO `report` (`r_id`, `type`, `results`, `s_received_date`, `s_tested_date`, `technician_remarks`, `fees`, `link_to_softcopy`, `t_id`, `NIC`) VALUES
+(1, 'scan', '', '2019-12-27 08:22:07', '2019-12-27 18:30:00', '', 2000, 'asceso.lab?id=123', 2001, '123456789v'),
+(2, 'scan', NULL, '2019-12-04 18:30:00', '2019-12-30 18:30:00', 'everything is fine', 2000, 'asceso.lab?id=234', 2001, '987654321v'),
+(3, 'scan', '', '2019-07-05 18:30:00', '2019-12-23 18:30:00', 'reports are normal', 1200, 'asceso.lab?id=001', 2000, '987654321v');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `staff`
+--
+
+DROP TABLE IF EXISTS `staff`;
+CREATE TABLE IF NOT EXISTS `staff` (
+  `b_id` int(11) NOT NULL AUTO_INCREMENT,
+  `d_id` int(11) NOT NULL,
+  PRIMARY KEY (`b_id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 COMMIT;
 
