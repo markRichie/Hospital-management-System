66ec8c1e2d48512dcbbcc9858cb1b6678f65c915
xringo <58860576+xringo@users.noreply.github.com>
2019-12-29 06:06:31 +0530

    Add files via upload

--- admin.php
+++ admin.php
@@ -1,15 +1,28 @@
 <?php
+
+// Start the session
 session_start();
-if(isset($_SESSION['Role']))
-{
-  if($_SESSION['Role'] != 'admin')
-  {
-    header('Location: clerk.php');
-  }
+$rle = $_SESSION["role"];
+
+if ($rle == "admin"){
+  $id =  $_SESSION["idi"];
 }
-else
+
+include "new_appointment.php";
+
+
+$connect = mysqli_connect("localhost", "root", "", "hospital_db");  
+$query ="SELECT * FROM `doctor`";  
+
+// for method 
+
+$result2 = mysqli_query($connect, $query);
+
+$options = "";
+
+while($row2 = mysqli_fetch_array($result2))
 {
-  header('Location: index.php');
+    $options = $options."<option>$row2[0]</option>";
 }
 
 ?>
@@ -23,40 +36,64 @@ integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9If
 <div class="jumbotron" style="background:url('images/Adminpanel.jpg') 
 no-repeat;background-size:cover;height:200px;"></div>
 <div class="container-fluid">
-  <?php echo $_SESSION['User']; ?>
-  <div class="row">
-    <div class="col-md-3">
-    <div class="list-group">
-    <a href="" class="list-group-item active" style="background-color:#C70039; 
-    color:white;border-color:#C70039">Appoiments</a>
-    <a href= "" class="list-group-item">pending admisson</a>
-    <a href= "" class="list-group-item">Manage Doctors</a>
-    <a href= "" class="list-group-item">Reports</a>
-    <a href= "" class="list-group-item">Prescription</a>
-    </div>
-    <hr>
-    <div class="list-group">
-    <a href="" class="list-group-item active" style="background-color:#C70039; 
-    color:white;border-color:#C70039">Stafff</a>
-    <a href= "" class="list-group-item">Staff Details</a>
-    <a href= "" class="list-group-item">Add New Staff</a>
-    <a href= "" class="list-group-item">Delete Staff</a>
-  
-    </div>
-    </div>
-<div class="col-md-8">
-<div class="card">
-    <div class="card-body" style="background-color:#C70039;color:pink;"></div>
-    <div class="card-body"></div>
-
-</div>
-</div>
-<div class="col-md-1"></div>
-</div>
-</div>
-
+<div class="container-fluid" style="margin-top:50px;">
+    <div class="row">
+  <div class="col-md-4">
+    <div class="list-group" id="list-tab" role="tablist">
+      <a class="list-group-item list-group-item-action active" id="list-doctor-list" data-toggle="list" href="#list-doctor" role="tab" aria-controls="home">Manage Doctor</a>
+      <a class="list-group-item list-group-item-action" id="list-clerk-list" data-toggle="list" href="#list-clerk" role="tab" aria-controls="profile">Manage Clerk</a>
+      <a class="list-group-item list-group-item-action" id="list-messages-list" data-toggle="list" href="#list-messages" role="tab" aria-controls="messages">Prescription</a>
+      <a class="list-group-item list-group-item-action" id="list-settings-list" data-toggle="list" href="#list-settings" role="tab" aria-controls="settings">Doctors Section</a>
+       <a class="list-group-item list-group-item-action" id="list-attend-list" data-toggle="list" href="#list-attend" role="tab" aria-controls="settings">Attendance</a>
+    </div><br>
+  </div>
+  <div class="col-md-8">
+    <div class="tab-content" id="nav-tabContent">
+      <div class="tab-pane fade show active" id="list-doctor" role="tabpanel" aria-labelledby="list-doctor-list">
+        <div class="container-fluid">
+          <div class="card">
+            <div class="card-body">
+              <center><h4>Add Doctor</h4></center><br>
+              <form class="form-group" action="callfunctions.php"  method="POST">
+                <div class="row">
+                  <div class="col-md-4"><label>Full Name:</label></div>
+                  <div class="col-md-8"><input type="text" class="form-control"  name="txtdoc_fname" placeholder="Enter Doctors Full Name"required></div><br><br>
+                  <div class="col-md-4"><label>Fees:</label></div>
+                  <div class="col-md-8"><input type="text"  class="form-control" name="txtdoc_fees" placeholder="Enter Doctors Fees"required></div><br><br>
+                  
+                  <div class="col-md-4">
+                    <input type="submit" name="btnadd_doc" value="Register Doctor" class="btn btn-primary" id="inputbtn">
+                    <input type="submit" formnovalidate name="btndelete_doc" value="Delete Doctor" class="btn btn-primary" id="inputbtn">
+                  </div>
+                  <div class="col-md-8"></div>                  
+                </div>
+              </form>
+            </div>
+          </div>
+        </div><br>
+      </div>
+      <div class="tab-pane fade" id="list-clerk" role="tabpanel" aria-labelledby="list-clerk-list">
+        <div class="card">
+          <div class="card-body">
+          <center><h4>Add Clerk</h4></center><br>
+              <form class="form-group" action="callfunctions.php"  method="POST">
+                <div class="row">
+                  <div class="col-md-4"><label>Full Name:</label></div>
+                  <div class="col-md-8"><input type="text" class="form-control"  name="txtclerk_fname" placeholder="Enter Clerks Full Name"required></div><br><br>
+                  <div class="col-md-4">
+                    <input type="submit" name="btnadd_clerk" value="Register Clerk" class="btn btn-primary" id="inputbtn">
+                    <input type="submit" formnovalidate name="btndelete_clerk" value="Delete Clerk" class="btn btn-primary" id="inputbtn">
+                  </div>
+                  <div class="col-md-8"></div>                  
+                </div>
+              </form>
+            </div>
+          </div>
+        </div><br>
+      </div>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" 
-integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>   
+integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+   
 </body>
 </html>
