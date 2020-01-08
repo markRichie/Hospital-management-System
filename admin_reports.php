6dd9fe45f0eea375589bc5844066c6d06533eb36
unknown <markrichie96@gmail.com>
2020-01-08 18:19:29 +0530

    modiefied

--- admin_reports.php
+++ admin_reports.php
@@ -1,90 +1,53 @@
-<?php
- 
- 
-$conn = mysqli_connect("localhost", "root", "","hospital_db");
-$query = mysqli_query($conn,"SELECT `name`,`qty` FROM medicine");
-
-while ($row = mysqli_fetch_array($query)) {
-
-}
-$dataPoints = array( 
-	array("y" => 3373.64, "label" => "Germany" )
-);
-$dataPoints = array( 
-	array("y" => 3373.64, "label" => "Germany" )
-);
- 
-?>
-<!doctype html>
-<html lang="en">
+<html>
   <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!--Load the AJAX API-->
+    <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+    <script type="text/javascript">
 
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="css/bootstrap.min.css">
+      // Load the Visualization API and the corechart package.
+      google.charts.load('current', {'packages':['corechart']});
 
-    <title>Hello, world!</title>
-    <style>
-      .headg{
-        margin-left: 30px;
-        margin-right: 30px;
+      // Set a callback to run when the Google Visualization API is loaded.
+      google.charts.setOnLoadCallback(drawChart);
+
+      // Callback that creates and populates a data table,
+      // instantiates the pie chart, passes in the data and
+      // draws it.
+      function drawChart() {
+
+        // Create the data table.
+        var data = new google.visualization.DataTable();
+        data.addColumn('string', 'Topping');
+        data.addColumn('number', 'Slices');
+        data.addRows([
+        <?php
+            $conn = mysqli_connect("localhost", "root", "","hospital_db");
+            $query = mysqli_query($conn,"select * from medicine");
+
+            $num = 1;
+            while ($row = mysqli_fetch_array($query)) {
+                $qty = $row['qty'];
+                $nme = $row['name'];
+                echo " [".$nme.", ".$qty."],";
+            }
+         
+        ?>
+        ]);
+
+        // Set chart options
+        var options = {'title':'medicine in stock',
+                       'width':400,
+                       'height':300};
+
+        // Instantiate and draw our chart, passing in some options.
+        var chart = new google.visualization.PieChart(document.getElementById('chart_div'));
+        chart.draw(data, options);
       }
-      h1{
-        margin-top: 20px;
-        margin-bottom: 20px;
-      }
-      #ph{
-        margin-top: 5px;
-      }
-      #nw{
-        margin-bottom: 20px;
-      }
-      #qty{
-        width: 50%;
-        margin-left: 20px;
-      }
-      #chartContainer{
-        margin-left: 30px;
-        margin-top: 30px;
-      }
-    </style>
-    <script>
-    
-        window.onload = function() {
-        
-        var chart = new CanvasJS.Chart("chartContainer", {
-            animationEnabled: true,
-            theme: "light2",
-            title:{
-                text: "medicine stock"
-            },
-            axisY: {
-                title: "qty"
-            },
-            data: [{
-                type: "column",
-                yValueFormatString: "#,##0.## tonnes",
-                dataPoints: <?php echo json_encode($dataPoints, JSON_NUMERIC_CHECK); ?>
-            }]
-        });
-        chart.render();
-        
-        }
     </script>
-    <div id="chartContainer" style="height: 370px; width: 80%;"></div>
-    <script src="https://canvasjs.com/assets/script/canvasjs.min.js"></script>
-
   </head>
+
   <body>
-  
-
-
-    <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+    <!--Div that will hold the pie chart-->
+    <div id="chart_div"></div>
   </body>
 </html>
--- data.php
+++ data.php
@@ -1,18 +1,28 @@
 <?php
+
 header('Content-Type: application/json');
 
-//require_once('db.php');
-$conn = mysqli_connect("localhost", "root", "","hospital_db");
-$sqlQuery = "SELECT `name`,`qty` FROM medicine";
+$con = mysqli_connect("localhost".","root","","hospital_db");
 
-$result = mysqli_query($conn,$sqlQuery);
-
-$data = array();
-foreach ($result as $row) {
-	$data[] = $row;
+// Check connection
+if (mysqli_connect_errno($con))
+{
+    echo "Failed to connect to DataBase: " . mysqli_connect_error();
+}else
+{
+    $data_points = array();
+    
+    $result = mysqli_query($con, "SELECT * FROM medicines");
+    
+    while($row = mysqli_fetch_array($result))
+    {        
+        $point = array("label" => $row['name'] , "y" => $row['qty']);
+        
+        array_push($data_points, $point);        
+    }
+    
+    echo json_encode($data_points, JSON_NUMERIC_CHECK);
 }
+mysqli_close($con);
 
-//mysqli_close($conn);
-
-echo json_encode($data);
 ?>
