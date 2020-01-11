b3c646a0ddf48e91c5ae8d739cf6745dc7b6f3a8
xringo <58860576+xringo@users.noreply.github.com>
2020-01-11 20:24:53 +0530

    Update request_reports.php

--- request_reports.php
+++ request_reports.php
@@ -75,9 +75,9 @@ $rle = $_SESSION["role"];
           <a class="nav-link" href="#"><span class="sr-only">(current)</span></a>
         </li>
       </ul>
-      <form class="form-inline my-2 my-lg-0">
-        <button class="btn btn-info my-2 my-sm-0" type="submit">Sign out</button>
-      </form>
+      <form class="form-inline my-2 my-lg-0" action="callfunctions.php"  method="POST">
+            <button class="btn btn-info my-2 my-sm-0"  name="btn_signout" type="submit">Sign out</button>
+          </form>
     </div>
   </nav>
   <div class="headg" >
